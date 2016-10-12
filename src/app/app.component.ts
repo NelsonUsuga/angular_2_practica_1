a15a83d75d74f32d6123679ec401bb334085214a
Nelson <nelson@debian-nelson>
2016-10-12 17:21:31 -0500

    12 de Octubre de 2016

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
+
+  nombre = 'Nelson';
+
+  fecha = '12 de Octubre de 2016';
+
+  oculto = true;
+
+  ocultar(oculto) {
+    this.oculto = oculto;
+  }
 }
