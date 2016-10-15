8caaef5871a8b9e1309fd306a1c51db4d663132a
Nelson <nelson@debian-nelson>
2016-10-15 11:39:33 -0500

    15 de Octubre de 2016

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,43 @@ export class AppComponent {
 
   fecha = '15 de Octubre de 2016';
 
+  // Propiedad ejemplo etiquetas html dinámicas
   oculto = true;
 
+  // Propiedad ejemplo de directiva ngModel
   textoprueba = 'Prueba de texto bidireccional';
 
-  // Ejemplo de directiva
+  // Propiedades ejemplo de directiva ngClass
   textosino = 'SI';
   estado = true;
 
+  // Propiedad ejemplo de directiva ngFor
+  animales = ['Perro', 'Gato', 'Caballo', 'Ratón', 'Elefante'];
+
+  // Propiedad ejemplo de directiva ngIf
+  mostrarImagen = false;
+
+  // Función ejemplo de directiva ngClass
   cambiarEstado() {
       this.estado = !this.estado;
       this.textosino = (this.estado) ? 'SI' : 'NO';
   }
-  // Fin ejemplo de directiva
 
+  // Función ejemplo de directiva ngFor
+  listarAnimales() {
+      return this.animales;
+  }
+
+  // Función ejemplo etiquetas html dinámicas
   ocultar(oculto) {
     this.oculto = oculto;
+  }
+
+  // Funciones ejemplo de directiva ngIf
+  sobreponerMouse() {
+      this.mostrarImagen = true;
+  }
+  retirarMouse() {
+      this.mostrarImagen = false;
   }
 }
