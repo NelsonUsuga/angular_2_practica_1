4375420bde5c9e8b841ded8cf2f5f0056fd00393
Nelson <nelson@debian-nelson>
2016-10-15 09:30:56 -0500

    15 de Octubre de 2016

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent {
@@ -11,11 +11,21 @@ export class AppComponent {
 
   nombre = 'Nelson';
 
-  fecha = '13 de Octubre de 2016';
+  fecha = '15 de Octubre de 2016';
 
   oculto = true;
 
   textoprueba = 'Prueba de texto bidireccional';
+
+  // Ejemplo de directiva
+  textosino = 'SI';
+  estado = true;
+
+  cambiarEstado() {
+      this.estado = !this.estado;
+      this.textosino = (this.estado) ? 'SI' : 'NO';
+  }
+  // Fin ejemplo de directiva
 
   ocultar(oculto) {
     this.oculto = oculto;
